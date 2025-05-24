327fdb12426cb03802a4a3121e602e84d1260d26
Developer <realstormceo@gmail.com>
2025-05-24 06:28:45 -0500

    Added Force Dynamic to all pages 2

--- appbot2.0-web/src/lib/db.js
+++ appbot2.0-web/src/lib/db.js
@@ -1,9 +1,9 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: 'localhost',
+  host: 'db1.tech2you.co.nz',
   user: 'root',
-  password: '', // your actual password if any
+  password: 'qhxcNj5JoU', // your actual password if any
   database: 'appbot2c_application_bot',
   waitForConnections: true,
   connectionLimit: 10,
